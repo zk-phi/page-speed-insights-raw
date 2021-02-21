878aafed7e1df5cb32af877459c1084a9b3acb35
zk-phi <zk-phi@users.noreply.github.com>
2021-02-21 18:21:23 +0900

    yey

--- main.js
+++ main.js
@@ -1,3 +1,61 @@
+const simulatedTimingKeys = [
+    "firstCPUIdle",
+    "firstContentfulPaint",
+    "firstMeaningfulPaint",
+    "largestContentfulPaint",
+    "interactive",
+];
+
+const observedTimingKeys = [
+    "observedTimeOrigin",
+    "observedNavigationStart",
+    "observedFirstContentfulPaint",
+    "observedFirstContentfulPaintAllFrames",
+    "observedFirstMeaningfulPaint",
+    "observedFirstPaint",
+    "observedFirstVisualChange",
+    "observedLargestContentfulPaint",
+    "observedLargestContentfulPaintAllFrames",
+    "observedDomContentLoaded",
+    "observedLoad",
+    "observedLastVisualChange",
+    "observedTraceEnd",
+];
+
+function simulatedTimings (lighthouseResult) {
+    if (!lighthouseResult) return [];
+
+    return simulatedTimingKeys.map((key) => ({
+        time: lighthouseResult.audits.metrics.details.items[0][key],
+        key: key,
+    })).sort((a, b) =>
+        a.time <= b.time ? -1 : a.time == b.time ? 0 : 1
+    );
+}
+
+function observedTimings (lighthouseResult) {
+    if (!lighthouseResult) return [];
+
+    const timings = observedTimingKeys.map((key) => ({
+        time: lighthouseResult.audits.metrics.details.items[0][key],
+        key: key,
+    }));
+
+    lighthouseResult.audits["network-requests"].details.items.forEach((item) => {
+        timings.push({
+            time: Math.floor(item.startTime),
+            key: `loadStart ${item.url}`,
+        });
+        timings.push({
+            time: Math.ceil(item.endTime),
+            key: `loadEnd ${item.url}`,
+        });
+    });
+
+    return timings.sort((a, b) =>
+        a.time <= b.time ? -1 : a.time == b.time ? 0 : 1
+    );
+}
 
 var vm = new Vue({
     el: "#app",
@@ -8,7 +66,13 @@ var vm = new Vue({
         result: null,
         loading: false
     },
-    mounted: function () {
+    computed: {
+        simulatedTimings: function () {
+            return simulatedTimings(this.result);
+        },
+        observedTimings: function () {
+            return observedTimings(this.result);
+        }
     },
     methods: {
         run: function () {
@@ -19,7 +83,7 @@ var vm = new Vue({
                 "&strategy=" + this.strategy
             );
             xhr.onload = function () {
-                vm.result = JSON.parse(xhr.responseText);
+                vm.result = JSON.parse(xhr.responseText).lighthouseResult;
                 vm.status = "Completed";
                 vm.loading = false;
             };
@@ -29,6 +93,7 @@ var vm = new Vue({
             };
             vm.loading = true;
             vm.status = "Loading ...";
+            vm.result = null;
             xhr.open("GET", url, true);
             xhr.send(null);
         }
